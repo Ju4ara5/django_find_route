0ec498055d05c617c266aac755b975e37780a6c9
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2022-06-27 10:06:05 +0300

    детализация записи - шаблон, урл адрес + кнопка

--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # функция отображения списка городов:
 from cities.models import City
@@ -8,7 +8,15 @@ __all__ = (
     'home',
 )
 
-def home(request):
+
+def home(request, pk=None):
+    if pk:
+        #  city = City.objects.filter(id=pk).first()  # выведит пустую страницу если не существует city
+        #  city = City.objects.get(id=pk)  # выведет ошибку если не существует city
+        city = get_object_or_404(City, id=pk)  # вернет ошибку 404
+        context = {'object': city}
+        return render(request, 'cities/detail.html', context)
+
     qs = City.objects.all()
     context = {'object_list': qs}
     return render(request, 'cities/home.html', context)
